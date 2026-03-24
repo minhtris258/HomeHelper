cd812f4c779e1dbbdb8782e04e50eb4b72082972
minhtris25 <tomleeq258@gmail.com>
2026-03-24 15:51:51 +0700

    a

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6bedc23c5db5ede70a7a3840baefed4aaf6cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eebccb00d4ec806b49f4da12cfd2f49f2af32cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
