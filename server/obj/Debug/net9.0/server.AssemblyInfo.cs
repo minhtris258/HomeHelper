b705c8dbc69131a61d687fc2a77302682742b03d
minhtris25 <tomleeq258@gmail.com>
2026-03-24 15:27:52 +0700

    Update project dependencies and target framework
    - Changed target framework from net10.0 to net9.0 in server.csproj.
    - Updated package references to version 9.0.0 for:
      - Microsoft.AspNetCore.Authentication.JwtBearer
      - Microsoft.EntityFrameworkCore.Design
      - Microsoft.EntityFrameworkCore.Sqlite
      - Microsoft.EntityFrameworkCore.Tools
      - Npgsql.EntityFrameworkCore.PostgreSQL
    - Adjusted SQLitePCLRaw.lib.e_sqlite3 dependency version from 2.1.11 to 2.1.10.
    - Added new dependencies for System.Text.Json, Microsoft.Extensions.Logging.Abstractions, and Microsoft.Extensions.Options at version 9.0.0.
    - Updated Microsoft.CodeAnalysis.Analyzers dependency version from 3.11.0 to 3.3.4.

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a4a1230196996820a9353a5e4c596a2efbf292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695b1ecf1f6140343cca61cdcb1f87ae90d42f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
