95fc864b337babe873d8cdefdd33b15b869e6122
minhtris25 <tomleeq258@gmail.com>
2026-03-24 14:40:14 +0700

    feat: add Dockerfile for multi-stage build and deployment setup

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cb9de7c625b9c3ba2caaa00c79b711e361fe93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8eb09e322e6f6f29dab8d39fdc89e805504438")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
